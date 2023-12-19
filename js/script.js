7dd264a541aba29aa14544839238e01989f435c9
BiagioCaputo <scebiagio98@gmail.com>
2023-12-19 09:48:18 +0100

    aggiunto il tofixed

--- js/script.js
+++ js/script.js
@@ -19,17 +19,19 @@ console.log("Costo biglietto senza sconto: " , price );
 //recupero il prezzo applicando eventualmente lo sconto
 
 if (userAge < 65 && userAge > 17){
-    finalPrice.innerText =  price;
+    finalPrice.innerText =  price.toFixed(2);
 }
 
 else{
 
     if (userAge < 18){
-        finalPrice.innerText = price - price * 20 / 100;
+        const price20 = price - price * 20 / 100;
+        finalPrice.innerText = price20.toFixed(2);
     }
     
     else{
-        finalPrice.innerText = price - price * 40 / 100;
+        const price40 = price - price * 40 / 100;
+        finalPrice.innerText = price40.toFixed(2); 
     }
     
 }
